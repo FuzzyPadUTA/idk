25750785485946049cf2f9154e5996a1c0c65b62
FuzzyPadUTA <FuzzyPadUTA@gmail.com>
2018-11-13 16:24:57 -0500

    Add files via upload

---
+++ templates/main.php
@@ -0,0 +1,82 @@
+<html>
+<!DOCTYPE html>
+<html>
+<head>
+<body style="background-color:white;">
+<title>My Drive-Google Drive</title>
+</script><link rel="icon" href="//ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_5.ico">
+
+<meta name="generator" content="php-proxy.com">
+<meta name="version" content="5.1.0">
+
+<style type="text/css">
+html body {
+	font-family: Arial,Helvetica,sans-serif;
+	font-size: 12px;
+}
+
+#container {
+	width:500px;
+	margin:0 auto;
+	margin-top:150px;
+}
+
+#error {
+	color:red;
+	font-weight:bold;
+}
+
+#frm {
+	padding:10px 15px;
+	background-color:#1f1f14;
+	
+	border:1px solid #6600ff;
+	
+	-webkit-border-radius: 8px;
+	-moz-border-radius: 8px;
+	border-radius: 8px;
+}
+
+#footer {
+	text-align:center;
+	font-size:10px;
+	margin-top:35px;
+	clear:both;
+}
+</style>
+
+</head>
+
+<body>
+
+
+<div id="container">
+    
+    <h3 style="color:blue;">F Z P X</h3>
+	</div>
+</center>
+		
+	<div id="frm">
+	
+	<!-- Don't touch. Bad -->
+	
+		<form action="index.php" method="post" style="margin-bottom:0;">
+			<input name="url" type="text" style="width:1300px;" autocomplete="off" placeholder="http://" />
+			<input type="submit" value="Go" />
+		</form>
+		
+		<script type="text/javascript">
+			document.getElementsByName("url")[0].focus();
+		</script>
+		
+	<!-- [END] -->
+	
+	</div>
+	
+</div>
+	<div style="text-align:center;">
+		<h2 style="color:green;">Discord server above</h2>
+	    </div
+<div id="footer">
+	Subscribe <a href="//www.youtube.com/fuzzypaduta" target="_blank">FZPX</a>
+</div>
